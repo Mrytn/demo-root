be0b5395733038a0341937684cc3bf53b34e7de9
PeiDong Yan <31368606+Mrytn@users.noreply.github.com>
2023-03-16 17:34:27 +0800

    修改

--- dubbo-product-md/src/main/java/com/example/dubboproductmd/DemoMdApplication.java
+++ dubbo-product-md/src/main/java/com/example/dubboproductmd/DemoMdApplication.java
@@ -1,9 +1,7 @@
 package com.example.dubboproductmd;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
 public class DemoMdApplication {
--- dubbo-product-md/src/main/java/com/example/dubboproductmd/impl/ProductServiceImpl.java
+++ dubbo-product-md/src/main/java/com/example/dubboproductmd/impl/ProductServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.dubboproductmd.impl;
 
 import com.example.dubboproductmd.ProductService;
+import org.apache.dubbo.config.annotation.DubboService;
 
 /**
  * @author PeiDong Yan
  * @date 2023/03/15 16:58
  */
-@DubboService("")
+@DubboService(interfaceClass = ProductService.class)
 public class ProductServiceImpl implements ProductService {
+
+
 }
